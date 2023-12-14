17f42fbbf04a55d2f0e9e47dcf7fdf90e1f03007
stanlyh <eduardostanly@gmail.com>
2023-12-13 23:49:49 -0500

    Estructura de carpetas, uso de tailwind

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Hola celeste</h1>
+  `
+})
+export class UsersLoaderComponent {
+  constructor() {}
+
+}
