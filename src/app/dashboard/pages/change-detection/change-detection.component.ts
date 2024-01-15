fbd2d1e000841ac87ebf8642e81c3fc79ffa6990
stanlyh <eduardostanly@gmail.com>
2024-01-15 16:02:58 -0500

    Change Detectión -OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
+  template: `
+
+  <app-title [title]="currentFramework()" />
+
+  <pre> {{ fameworkAsSignal() | json }} </pre>
+  <pre> {{ fameworkAsProperty | json }} </pre>
+
+  `
 })
 export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change detection - ${ this.fameworkAsSignal().name }`
+  );
+
+  public fameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  })
+
+  public fameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+
+    setTimeout(() => {
+
+      // this.fameworkAsProperty.name = 'React';
+      this.fameworkAsSignal.update( value => ({
+        ...value,
+        name: 'React'
+      }))
+
+      console.log('Hecho')
+    }, 3000);
+
+  }
 
 }
