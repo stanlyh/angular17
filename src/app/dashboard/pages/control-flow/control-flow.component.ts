533f6b0a441c0ae3ca4b789d87d9ce372e7f8589
stanlyh <eduardostanly@gmail.com>
2024-01-15 15:06:22 -0500

    @switch y @for

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+type Grade = 'A'|'B'|'F';
+
 @Component({
   standalone: true,
   imports: [CommonModule],
@@ -10,6 +12,10 @@ import { CommonModule } from '@angular/common';
 export default class ControlFlowComponent {
 
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
+
+  public frameworks = signal(['Angular','Vue','Svelte','Qwik','React']);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update( value => !value );
