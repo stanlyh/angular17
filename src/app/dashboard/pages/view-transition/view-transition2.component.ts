22cfd9ba0e63779278577832725ea72121ba1e7f
stanlyh <eduardostanly@gmail.com>
2024-01-18 22:02:12 -0500

    view transition

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, TitleComponent],
+  template: `
+
+    <app-title title="View trasition 2"/>
+
+    <section class="flex justify-end">
+
+      <img
+        srcset="https://picsum.photos/id/237/200/300"
+        alt="Picsum"
+        width="200"
+        height="300"
+        style="view-transition-name: hero1"
+      />
+
+      <div
+        class="bg-blue-800 w-32 h-32 rounded"
+        style="view-transition-name: hero2"
+      >
+      </div>
+
+    </section>
+
+  `
+})
+export default class ViewTransitionComponent {
+
+}
