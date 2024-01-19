9137bc5abd8c7e08e088dd52f812103f48b9f9b9
stanlyh <eduardostanly@gmail.com>
2024-01-19 16:58:57 -0500

    Servicio con señales

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsersService } from '@services/users.service';
+
 
 @Component({
   standalone: true,
@@ -8,5 +10,7 @@ import { CommonModule } from '@angular/common';
   styles: ``
 })
 export default class UsersComponent {
+
+  public usersService = inject( UsersService )
 
 }
